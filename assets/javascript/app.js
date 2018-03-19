b7655cdeb8018b5ee3827a0cb1821c562d18805e
Mark Hainline <mark.hainline@fakemail.com>
2018-03-19 16:25:26 -0400

    added main function

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,21 +1,33 @@
 // APP.JS
 
 // GLOBAL VARIABLES
+// =========================================================================
+var question_1 = [];
+
 
 // GLOBAL OBJECTS
 
-var timer = {
+//var timer = {
   // this provides the countdown for the game
-  time: 42,
-  loggit: function() {
-    console.log("I'm logging it!");
-    conso
-  },
-  countdown: function() {
-    time--;
-    console.log(timer.time);
-  }
-};
+  //time: 42,
+  //loggit: function() {
+  //  console.log("I'm logging it!");
+  //},
+  //countdown: function() {
+    //time--;
+    //console.log(timer.time);
+  //}
+//};
+
+// FUNCTIONS
+// =========================================================================
+
+function main() {
+  //timer.loggit();
+  //timer.countdown();
+  console.log("this works");
+}
+
 
 
 /* RESUME
@@ -24,10 +36,15 @@ timer.time isn't working, as shown by timer.countdown. Why?
 
 // GAME
 
-timer.loggit();
-timer.countdown();
+$(document).ready(function() {
+	main();
+})
+
+
 
 // DATA
 // TODO: move to a file and load into the game (stretch) - try jquery's .load() method
 
-question_1 = ["Q: What is the name of the computer shown as the background to this game?", "ENIAC", "UNIVAC", "CANIAC", "MANIAC", "Colossus"];
+// question_1 = ["Q: What is the name of the computer shown as the background to this game?", "ENIAC", "UNIVAC", "CANIAC", "MANIAC", "Colossus"];
+
+question_1 = [1, 2, 3, 4, 5];
